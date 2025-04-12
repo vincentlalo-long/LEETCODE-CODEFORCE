cfafe5654d2576b755e6b03c7ced51d633e72b09
Nguyễn Đăng Long <nickmoi3056@gmail.com>
2025-04-13 00:07:17 +0700

    Update Fibonanci.py

--- Algo/DynamicProgram/Fibonanci.py
+++ Algo/DynamicProgram/Fibonanci.py
@@ -1,1 +1,14 @@
-#Edit later
+#Code C : 
+int fib(int n){
+    int dp_0=0;
+    int dp_1=1;
+    int dp_ans;
+    if(n==0) return 0; # Simple case
+    if(n==1) return 1; # Simple case
+    for(int i=2;i<=n;i++){
+        dp_ans=dp_0+dp_1; #Caculate Fib Numbers
+        dp_0=dp_1;
+        dp_1=dp_ans;
+    }
+    return dp_ans;
+}
