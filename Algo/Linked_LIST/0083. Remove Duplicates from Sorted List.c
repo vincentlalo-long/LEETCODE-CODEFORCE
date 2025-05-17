64bfa2cc6012fc53a388c5d01a63fd4f6db109b9
Nguyễn Đăng Long <nickmoi3056@gmail.com>
2025-05-17 23:17:27 +0700

    Create 0083. Remove Duplicates from Sorted List.c

---
+++ Algo/Linked_LIST/0083. Remove Duplicates from Sorted List.c
@@ -0,0 +1,26 @@
+// Link : https://leetcode.com/problems/remove-duplicates-from-sorted-list/?envType=problem-list-v2&envId=linked-list
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    if(head==NULL || head->next==NULL){
+        return head;
+    }
+    struct ListNode* current=head;
+    while(current!=NULL&&current->next!=NULL){ 
+        if(current->val==current->next->val){ // xét 2 phần tử liên tiếp tại dãy đã sort rồi
+            struct ListNode* temp=current->next;
+            current->next=current->next->next; 
+            free(temp);
+        }
+        else{
+            current=current->next;
+        }
+    }
+    return head;
+
+}
