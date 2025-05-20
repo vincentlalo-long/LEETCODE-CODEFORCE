39824dfcd3e87032ad1c42989d1b3b9997840452
Nguyễn Đăng Long <nickmoi3056@gmail.com>
2025-05-20 09:59:57 +0700

    Create 0020. Valid Parentheses.c

---
+++ Leetcode/0020. Valid Parentheses.c
@@ -0,0 +1,25 @@
+//Link : https://leetcode.com/problems/valid-parentheses/description/?envType=problem-list-v2&envId=stack
+// Code C : 
+bool isValid(char* s) {
+    char stack[10000];
+    int top=-1;
+    // Code để push phần tử vào stack : 
+    //int len=strlen(s);
+    for(int i=0;s[i]!='\0';i++){
+        //stack[++top]=char[i];
+        if(s[i]=='('||s[i]=='{'||s[i]=='['){
+            stack[++top]=s[i];
+        }
+        else{
+            if(top==-1) return false;
+            char open=stack[top--];
+            if(open=='('&&s[i]!=')'||
+               open=='{'&&s[i]!='}'||
+               open=='['&&s[i]!=']'){
+                return false;
+               }
+        }
+    }
+    return (top==-1);
+
+}
