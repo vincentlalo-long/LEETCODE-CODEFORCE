cb02959d54c6916b9c57398bd11126876ebf9b8b
Nguyễn Đăng Long <nickmoi3056@gmail.com>
2025-05-22 10:17:49 +0700

    Update 0024. Swap Nodes in Pairs.c

--- Leetcode/0024. Swap Nodes in Pairs.c
+++ Leetcode/0024. Swap Nodes in Pairs.c
@@ -24,3 +24,22 @@ struct ListNode* swapPairs(struct ListNode* head) {
     }
     return dummyNode.next; // trả về danh sashc liên kết
 }
+// Cách khác : đệ quy : 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* swapPairs(struct ListNode* head) {
+    // Ta sẽ giải bằng phương pháp đệ quy 
+    // Đầu tiên ta giải các TH base case 
+    if(head==NULL || head->next==NULL) return head; // nếu không có node nào hoặc chỉ có duy nhất 1 node thì trả về 
+    // Khi có 2 node trở lên thì ta sẽ đệ quy để swap
+    struct ListNode* first=head;
+    struct ListNode* second=first->next;
+    first->next=swapPairs(second->next);
+    second->next=first;
+    return second;
+}
