3d913490fe7a615c7ea5f5d4bb1785eb9bf44a5b
Nguyễn Đăng Long <nickmoi3056@gmail.com>
2025-07-09 22:05:20 +0700

    Create 0104.Maximum Depth of Binary Tree.c

---
+++ Algo/Binary Tree/0104.Maximum Depth of Binary Tree.c
@@ -0,0 +1,7 @@
+#define max(a,b) ((a) > (b) ? (a) : (b))
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL) return 0;
+    int lr=maxDepth(root->left);
+    int rr=maxDepth(root->right);
+    return 1 + max(lr,rr);
+}
